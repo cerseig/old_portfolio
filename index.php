38a0635f6b3d175d59e3c328ddd256bd9a11394d
Circé <circegrand@gmail.com>
2016-12-22 00:56:02 +0100

    Home + about

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 <main id="home" class="home" role="main">
     <section class="landing">
+        <div class="container">
+        <div class="particles" id="particles-js"></div>
         <div class="block_noir">
             <div class="content">
                 <div class="title">
@@ -12,6 +14,19 @@
                 <p class="resume">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
         </div>
+    </div>
+    </section>
+    <section class="about">
+        <div class="container">
+        <div class="content">
+            <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
+            <div class="text">
+                <h3>A dynamic french <br>developper</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
+            </div>
+        </div>
+    </div>
     </section>
 </main>
 
