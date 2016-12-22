f4682233333f12b5eef33079597c9d0c2ac97f4d
Circé <circegrand@gmail.com>
2016-12-22 22:49:54 +0100

    mobile: home+about

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
             <div class="content">
                 <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
                 <div class="text">
-                    <h3>A dynamic french <br>developper</h3>
+                    <h3>A dynamic french <br>web developper</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
                     <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
                 </div>
@@ -38,16 +38,9 @@
     </section>
 
 <!-- SECTION PROJETS -->
-    <section class="about">
+    <section class="projects">
         <div class="container">
-            <div class="content">
-                <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
-                <div class="text">
-                    <h3>A dynamic french <br>developper</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                    <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
-                </div>
-            </div>
+
         </div>
     </section>
 
