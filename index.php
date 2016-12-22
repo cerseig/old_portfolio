44d7724a205a77c30103a5b281787b88136ef200
Circé <circegrand@gmail.com>
2016-12-22 22:37:51 +0100

    mobile: about

--- index.php
+++ index.php
@@ -5,37 +5,52 @@
 <!-- SECTION PAGE D'ARRIVÉE -->
     <section class="landing">
         <div class="container">
-        <div class="particles" id="particles-js"></div>
-        <div class="block_noir">
-            <div class="content">
-                <div class="triangles">
-                    <img src="assets/img/triangle.png" alt="">
-                    <img src="assets/img/triangle-contour.png" alt="">
-                </div>
-                <div class="title">
-                    <h1 class="name">Circé Grand</h1>
-                    <h2 class="role">Creative Developper</2>
-                </div>
-                <hr>
-                <p class="resume">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div class="particles" id="particles-js"></div>
+                <div class="block_noir">
+                    <div class="content">
+                        <div class="triangles">
+                            <img src="assets/img/triangle.png" alt="">
+                            <img src="assets/img/triangle-contour.png" alt="">
+                        </div>
+                        <div class="title">
+                            <h1 class="name">Circé Grand</h1>
+                            <h2 class="role">Creative Developper</2>
+                        </div>
+                        <hr>
+                        <p class="resume">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
             </div>
         </div>
-    </div>
     </section>
 
 <!-- SECTION A PROPOS -->
     <section class="about">
         <div class="container">
-        <div class="content">
-            <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
-            <div class="text">
-                <h3>A dynamic french <br>developper</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
+            <div class="content">
+                <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
+                <div class="text">
+                    <h3>A dynamic french <br>developper</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                    <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
+                </div>
             </div>
         </div>
-    </div>
     </section>
+
+<!-- SECTION PROJETS -->
+    <section class="about">
+        <div class="container">
+            <div class="content">
+                <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
+                <div class="text">
+                    <h3>A dynamic french <br>developper</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                    <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </main>
 
 <?php require 'footer.php' ?>
