b6a7732b44d1fbc13b215e08d2acf976032727d1
Circé <circegrand@gmail.com>
2017-01-04 11:43:45 +0100

    Nav Hover

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                         </div>
                         <div class="title">
                             <h1 class="name">Circé Grand</h1>
-                            <h2 class="role">Creative Developper</2>
+                            <h2 class="role">Creative Developer</2>
                         </div>
                         <hr>
                         <p class="resume">Hello ! I am  a student in second yeard of my two-year university Degree of Techonology specialised in Multimedia and the Internet. I am a creative developper and I am looking for an internship as web integrator for 3 months in Paris (or around)</p>
@@ -31,7 +31,7 @@
             <div class="content">
                 <img src="assets/img/test-brush.png" alt="Photo de la section A Propos">
                 <div class="text">
-                    <h3>A dynamic french <br>web developper</h3>
+                    <h3>A dynamic french <br>web developer</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
                     <a href="about.php" alt="Etre redirigé sur la page A Propos">En savoir plus</a>
                 </div>
@@ -84,6 +84,7 @@
                     <li><a href="https://twitter.com/circegrand_pro" alt="Redirection vers Twitter"><i class="fa fa-twitter"></i></a></li>
                     <li><a href="https://fr.linkedin.com/in/circegrand" alt="Redirection vers Linkedin"><i class="fa fa-linkedin"></i></a></li>
                     <li><a href="http://www.viadeo.com/p/002r2kearrmvi16" alt="Redirection vers Viadeo"><i class="fa fa-viadeo"></i></a></li>
+                    <li><a href="https://github.com/cerseig" alt="Redirection vers Github"><i class="fa fa-github"></i></a></li>
                     <li><a href="https://www.youtube.com/channel/UCvx4lsTwaR_r_F_Z63BQYRA" alt="Redirection vers Youtube"><i class="fa fa-youtube"></i></a></li>
                     <li><a href="https://www.flickr.com/photos/circe_g/" alt="Redirection vers Flickr"><i class="fa fa-picture-o"></i></a></li>
                 </ul>
