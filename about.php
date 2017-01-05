5aa5538be5db00018e25a71dcc999c8bef4ebcf1
Circé <circegrand@gmail.com>
2017-01-05 18:51:59 +0100

    Responsive About 100%

--- about.php
+++ about.php
@@ -20,10 +20,10 @@
                     </div>
                     <div class="column column-2">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <a href="assets/cv/cvgraphique.pdf" alt="Redirection vers le CV Graphique"><div class="cv">CV graphique</div></a>
+                        <a href="assets/cv/cvbasique.pdf" alt="Redirection vers le CV Basique"><div class="cv">CV basique</div></a>
                     </div>
                 </div>
-                <a href="assets/cv/cvgraphique.pdf" alt="Redirection vers le CV Graphique"><div class="cv">CV graphique</div></a>
-                <a href="assets/cv/cvbasique.pdf" alt="Redirection vers le CV Basique"><div class="cv">CV basique</div></a>
             </div>
         </div>
     </section>
